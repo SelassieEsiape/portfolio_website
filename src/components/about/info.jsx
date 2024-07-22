6c730f04f77c513defd03f47f467cb82d1645d2a
Selassie Esiape <95718642+SelassieEsiape@users.noreply.github.com>
2024-07-21 23:00:11 -0600

    Corrected "class" to "className"

--- src/components/about/info.jsx
+++ src/components/about/info.jsx
@@ -18,7 +18,7 @@ const Info = () => {
         </div>
 
         <div className="about__box">
-        <i class='bx bx-certification about__icon'></i>
+        <i className='bx bx-certification about__icon'></i>
 
             <h3 className="about__title">Certifications</h3>
             <span className="about__subtitle">1</span>
